81ebb3325b7404b1a7fa78901239b7699da0149c
Vincent Dao <vincentdao288@gmail.com>
2025-05-06 14:25:25 +1000

    Added webkit autofill adjustment

--- main.js
+++ main.js
@@ -1,38 +1,3 @@
-/*
-
-  Logic: Required is already set in the HTMl
-         Create emailInput id = emailInput
-         Create userEmailConfirmation id = userEmailConfirmation
-         
-         Create mainLoginPage id = mainLoginPage
-         Create successPage id = successPage
-
-         Create submitBtn id = submitBtn
-         Create dismissBtn id = dismissBtn
-         
-         emailInput.addEventListener('input', (e) => {
-          Check for typeMismatch (i.e., emailInput.validity.typeMismatch)
-          If invalid: (i.e., type is CORRECT)
-            submitBtn.addEventListener('submit', (e) => {
-              e.preventDefault
-              
-              userEmailConfirmation.textContent = emailInput.textContent;
-
-              remove mainLoginPage
-              display successPage
-            })
-          Else:
-            emailInput.setCustomValidity("Valid email required");
-            set emailInput to have border red and background lighter red, text red like border too
-         })
-
-         dismissBtn.addEventListener("click", (e) => {
-          successPage.style.display = "none";  
-          mainPageLogin.style.display = "block";
-         })
-
-*/
-
 let form = document.getElementById("formContainer");
 let emailInput = document.getElementById("emailInput");
 let errorMsg = document.getElementById("errorMsg");
@@ -41,7 +6,6 @@ let mainLoginPage = document.getElementById("mainLoginPage");
 let successPage = document.getElementById("successPage");
 let submitBtn = document.getElementById("submitBtn");
 let dismissBtn = document.getElementById("dismissBtn");
-let emailCheck = "";
 
 form.addEventListener("submit", (e) =>{
   e.preventDefault();
@@ -55,13 +19,6 @@ form.addEventListener("submit", (e) =>{
     else if(emailInput.validity.valueMissing){
       errorMsg.textContent = "Input field required";
     }
-    form.classList.add("error-State");
-    return;
-  }
-
-  emailCheck = emailInput.value;
-  if(!emailCheck.endsWith(".com")){
-    errorMsg.textContent = "Valid email required";
     form.classList.add("error-State");
     return;
   }
